0e1b9290442215a69cfb334dc64f869a26af4ff4
Keivan <k1rezaei78@gmail.com>
2020-07-18 15:49:15 +0430

    privacy .... :)

--- CodeGen/Error.py
+++
@@ -1,8 +0,0 @@
-### Error Haro Ba Peygham Bezarid Taa Debug Rahat tar she.
-def error(message):
-    print(message)
-    exit(0)
-
-class TypeError(Exception):
-    def __init__(self, message):
-        self.message = message
